d3049afd8289cd560c78a1968b45fadeda528258
Sumiko Saito <sumiko@saitoujunkonoMacBook-ea.local>
2018-08-07 13:58:14 +0900

    delete mail to

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -1,7 +1,7 @@
 class BlogMailer < ApplicationMailer
   def blog_mail(blog)
-  @blog = blog
+    @blog = blog
 
- mail to: "sumiko_saito@diveintocode.jp", subject: "ブログ作成の確認メール"
-end
+    mail subject: "ブログ作成の確認メール"
+  end
 end
